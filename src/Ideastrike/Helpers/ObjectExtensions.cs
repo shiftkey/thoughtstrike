724d61f423a791bc9db6acf6092aba2e26c9a19c
aeoth <paul@theleagueofpaul.com>
2012-03-29 12:19:43 +1100

    Back end for image uploading (#8) done, somebody else want to handle the JS/front end?

--- src/Ideastrike/Helpers/ObjectExtensions.cs
+++ src/Ideastrike/Helpers/ObjectExtensions.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using System.Web.Routing;
 
 namespace Ideastrike.Helpers
@@ -14,6 +16,51 @@ namespace Ideastrike.Helpers
             foreach (var item in anonymousDictionary)
                 expando.Add(item);
             return (ExpandoObject)expando;
+        }
+
+        //http://stackoverflow.com/questions/1080442/how-to-convert-an-stream-into-a-byte-in-c
+        public static byte[] ReadToEnd(this Stream stream)
+        {
+            long originalPosition = stream.Position;
+            stream.Position = 0;
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                stream.Position = originalPosition;
+            }
         }
     }
 }
